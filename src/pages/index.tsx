28254234d5b80610c6b632fa693daf2fe862c6fe
dev-viniciuss <dev.mvsc@gmail.com>
2021-06-27 16:21:15 -0300

    feat: adicionando o botão Subscribe now na Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 
+import { SubscribeButton } from '../components/SubscribeButton'
+
 import styles from './home.module.scss'
 
 export default function Home() {
@@ -22,6 +24,8 @@ export default function Home() {
             Get acess to all publications <br />
             <span>for $9.90 month</span>
           </p>
+
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
