090005da2f4f7b6734a71961c3f7f4e572fc160d
dev-viniciuss <dev.mvsc@gmail.com>
2021-06-28 15:46:50 -0300

    feat: consumindo a api do strape e formatando moeda

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,24 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
 import avatarImg from '../../public/images/avatar.svg'
 
+import { stripe } from '../services/stripe'
+
 import { SubscribeButton } from '../components/SubscribeButton'
 
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: string
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+  
   return (
     <>
       <Head>
@@ -25,7 +36,7 @@ export default function Home() {
           
           <p>
             Get acess to all publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
           <SubscribeButton />
@@ -35,4 +46,25 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1J7K39CZUobL0B9tJKGCjXgc', {
+    expand: ['product']
+  }) 
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+
+    props: {
+      product
+    }
+  }
 }
