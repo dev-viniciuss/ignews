26f2c5a53b04afe36a39c7709c65d7d51bbaf2a9
dev-viniciuss <dev.mvsc@gmail.com>
2021-06-28 16:06:00 -0300

    feat: atualizando a chamda pro servidor pra static site generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
@@ -48,7 +48,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1J7K39CZUobL0B9tJKGCjXgc') 
 
   const product = {
@@ -60,9 +60,9 @@ export const getServerSideProps: GetServerSideProps = async () => {
   }
 
   return {
-
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
