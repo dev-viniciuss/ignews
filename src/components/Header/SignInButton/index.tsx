4996c262a914cf566e4171b67de824417e068150
dev-viniciuss <dev.mvsc@gmail.com>
2021-06-30 10:05:12 -0300

    WIP: Autenticação com o Github

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -1,24 +1,31 @@
+import { signIn, signOut, useSession } from 'next-auth/client'
+
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const isUserLoggedIn = false;
+  const [session] = useSession()
 
-  return isUserLoggedIn ? (
+  console.log(session);
+  
+
+  return session ? (
     <button 
       className={styles.signInButton}
       type="button"
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
-      Marcus Vinicius
+      {session.user.name}
       <FiX color="#737380"/>
     </button>
   ) : (
     <button 
       className={styles.signInButton}
       type="button"
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign in with Github
