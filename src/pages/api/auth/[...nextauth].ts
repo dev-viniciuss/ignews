a2435daf0baa027b08865326c4629a3d47dbacbf
dev-viniciuss <dev.mvsc@gmail.com>
2021-07-01 17:09:43 -0300

    feat: tratando de não adicionar um usuário duas vezes

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -13,19 +13,31 @@ export default NextAuth({
     }),
   ],
 
-  jwt: {
-    signingKey: process.env.SIGNING_KEY,
-  },
-
   callbacks: {
     async signIn(user) {
       const { email } = user
 
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            { data: { email } }
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
           )
         )
 
